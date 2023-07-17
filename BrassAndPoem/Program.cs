51269f8eff3d9043f29200d66d3d8ddb6b3f2ac9
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-07-17 18:58:42 -0500

    Created Product and Product Type lists

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,7 +1,53 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+List<Product> products = new List<Product>()
+{
+    new Product()
+    {
+        Name = "Trumpet",
+        Price = 5.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Trombone",
+        Price = 2.95M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Tuba",
+        Price = 4.95M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Ozymandias",
+        Price = 1.95M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Leaves of Grass",
+        Price = 9.95M,
+        ProductTypeId = 2
+    }
+};
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType()
+    {
+        Title="Brass",
+        Id=1
+    },
+        new ProductType()
+    {
+        Title="Poem",
+        Id=2
+    }
+};
 
 //put your greeting here
 
